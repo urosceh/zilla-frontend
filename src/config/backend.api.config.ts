dcd8fcaf7614de18b6102f16a97d55057f3ada56
urosceh <urosvceh@gmail.com>
2024-10-20 13:56:46 +0200

    Fix env variables

--- src/config/backend.api.config.ts
+++ src/config/backend.api.config.ts
@@ -1,8 +1,8 @@
 export const BackendApiConfig = {
-  _protocol: process.env.BACKEND_API_PROTOCOL || "http",
-  _host: process.env.BACKEND_API_HOST || "localhost",
-  _port: process.env.BACKEND_API_PORT || "3000",
-  _basePath: process.env.BACKEND_API_BASE_PATH || "api",
+  _protocol: process.env.REACT_APP_API_PROTOCOL || "http",
+  _host: process.env.REACT_APP_API_HOST || "localhost",
+  _port: process.env.REACT_APP_API_PORT || "3000",
+  _basePath: process.env.REACT_APP_API_BASE_PATH || "api",
 
   getBaseUrl: function (): string {
     return `${this._protocol}://${this._host}:${this._port}/${this._basePath}`;
