d9b71a4efc47a9ccd4d1128bd4d28eefa1458d0b
urosceh <urosvceh@gmail.com>
2025-10-21 08:58:15 +0200

    use nginx_url if set

--- src/config/backend.api.config.ts
+++ src/config/backend.api.config.ts
@@ -1,10 +1,12 @@
 export const BackendApiConfig = {
-  _protocol: process.env.REACT_APP_API_PROTOCOL || "http",
-  _host: process.env.REACT_APP_API_HOST || "localhost",
-  _port: process.env.REACT_APP_API_PORT || "3000",
-  _basePath: process.env.REACT_APP_API_BASE_PATH || "api",
+  _protocol: process.env.REACT_APP_API_PROTOCOL ?? "http",
+  _host: process.env.REACT_APP_API_HOST ?? "localhost",
+  _port: process.env.REACT_APP_API_PORT ?? "3000",
+  _basePath: process.env.REACT_APP_API_BASE_PATH ?? "api",
+
+  _nginxUrl: process.env.REACT_APP_NGINX_URL,
 
   getBaseUrl: function (): string {
-    return `${this._protocol}://${this._host}:${this._port}/${this._basePath}`;
+    return this._nginxUrl ? this._nginxUrl : `${this._protocol}://${this._host}:${this._port}/${this._basePath}`;
   },
 };
